2a8eda2b6bccc4963b8f8177909bfa201c93ce1a
Diego Bermudez <bermudezjimenezjuandiego@gmail.com>
2026-07-19 12:10:30 -0500

    added details to game definition

--- game/internal/definition/game_definition.go
+++ game/internal/definition/game_definition.go
@@ -1,20 +1,5 @@
 package definition
 
-type numericExpression string
-type boolExpression string
-type flowMutation string
-type globalMutation string
-type flowPlayerMutation string
-
-// expression that receives the available context and returns a single value which must be of the
-// same type of the referenced field
-type valueAssignmentExpression string
-
-// expression that receives the available context (if for global event then only global and the player's state
-// if for flow then it also includes flow state and player's state in that flow) and returns bool
-// true if the player should be assigned or false if not, expression is ran for every player
-type playersSelectionExpression string
-
 type dataType string
 
 // defines data types names
@@ -27,40 +12,52 @@ const (
 	listType    dataType = "list"
 )
 
+type operation string
+
 // defines the operations names
 const (
-	forEachOp               string = "for_each"
-	ifConditionOp           string = "if"
-	scopeVariableCreationOp string = "scope_variable"
-	assignmentOp            string = "assignment"
+	forEachOp               operation = "for_each"
+	ifConditionOp           operation = "if"
+	scopeVariableCreationOp operation = "scope_variable"
+	assignmentOp            operation = "assignment"
 )
+
+type interactionType string
 
 // defines interactioner types
 const (
-	timerInteraction     string = "timer"
-	userInteraction      string = "user_input"
-	waitGroupInteraction string = "wait_group"
+	timerInteraction     interactionType = "timer"
+	userInteraction      interactionType = "user_input"
+	waitGroupInteraction interactionType = "wait_group"
 )
+
+type conditionOperation string
 
 // defines condition connectors
 const (
-	greaterConnector string = "GREATER_THAN"
-	lowerConnector   string = "LOWER_THAN"
-	equalConnector   string = "EQUAL_TO"
+	greaterOp conditionOperation = "GREATER_THAN"
+	lowerOp   conditionOperation = "LOWER_THAN"
+	equalOp   conditionOperation = "EQUAL_TO"
 )
+
+type numericOperation string
 
 // defines numeric expression operations
 const (
-	plusOp  string = "PLUS"
-	minusOp string = "MINUS"
-	timesOp string = "TIMES"
-	divOp   string = "DIV"
+	plusOp  numericOperation = "PLUS"
+	minusOp numericOperation = "MINUS"
+	timesOp numericOperation = "TIMES"
+	divOp   numericOperation = "DIV"
 )
+
+type stringOperation string
 
 // defines string expression operations
 const (
-	joinWithOp string = "JOIN_WITH"
+	joinWithOp stringOperation = "JOIN_WITH"
 )
+
+type listOperation string
 
 // defines list operations
 const (
@@ -71,10 +68,12 @@ const (
 	removeOp string = "REMOVE"
 )
 
-// defines bool expression operations
+type boolConnector string
+
+// defines bool connectors
 const (
-	andOp string = "AND"
-	orOp  string = "OR"
+	andConnector boolConnector = "AND"
+	orConnector  boolConnector = "OR"
 )
 
 // defines wait connectors
@@ -85,11 +84,11 @@ const (
 
 type Game struct {
 	// Resources is the string of the the marshaled resources
-	Resources string
+	Resources ValueType
 	// PerPlayerState is the string of the marshaled per player state
-	PerPlayerStateSchema string
+	PerPlayerStateSchema ValueType
 	// RuntimeStateSchema marshaled schema for the mutable runtime schema
-	RuntimeStateSchema string
+	RuntimeStateSchema ValueType
 }
 
 type Status struct {
@@ -106,15 +105,15 @@ type StatusChanger struct {
 }
 
 type Interactioner interface {
-	GetInteractionType() string
+	GetInteractionType() interactionType
 }
 
 type InteractionGroup struct {
 	Interactions []Interactioner
-	Connectors   []string
+	Connectors   []boolConnector
 }
 
-func (g *InteractionGroup) GetInteractionType() string {
+func (g *InteractionGroup) GetInteractionType() interactionType {
 	return waitGroupInteraction
 }
 
@@ -122,7 +121,7 @@ type TimerInteraction struct {
 	Seconds uint
 }
 
-func (t *TimerInteraction) GetInteractionType() string {
+func (t *TimerInteraction) GetInteractionType() interactionType {
 	return timerInteraction
 }
 
@@ -130,81 +129,191 @@ type UserInputInteraction struct {
 	Params []Param
 	// these assignments can only perform assignments from the params received into the state
 	ThenWriteInState []AssignmentOp
+	// state field which contains ref to the users
+	UsersToInteractWithField RefType
 }
 
-func (t *UserInputInteraction) GetInteractionType() string {
+func (t *UserInputInteraction) GetInteractionType() interactionType {
 	return userInteraction
 }
 
 type Operation interface {
-	GetOperationType() string
+	GetOperationType() operation
 }
 
 type ForEachOp struct {
-	ListName    string
+	List        RefType
 	ItemName    string
 	IterationOp Operation
 }
 
-func (f *ForEachOp) GetOperationType() string {
+func (f *ForEachOp) GetOperationType() operation {
 	return forEachOp
 }
 
 type IfConditionOp struct {
 	BoolExpressions []Expression
-	Connectors      []string
+	Connectors      []boolConnector
 	IfTrue          Operation
 	IfFalse         Operation
 }
 
-func (f *IfConditionOp) GetOperationType() string {
+func (f *IfConditionOp) GetOperationType() operation {
 	return ifConditionOp
 }
 
 type ScopeVariableCreationOp struct {
 	VariableName string
-	Value        string
+	Value        ValueType
 	Op           Operation
 }
 
-func (v *ScopeVariableCreationOp) GetOperationType() string {
+func (v *ScopeVariableCreationOp) GetOperationType() operation {
 	return scopeVariableCreationOp
 }
 
 type AssignmentOp struct {
-	Field string
-	Value string
+	Field RefType
+	Value Expression
 }
 
-func (v *AssignmentOp) GetOperationType() string {
+func (v *AssignmentOp) GetOperationType() operation {
 	return assignmentOp
 }
 
-type Expression struct {
-	DataType  dataType
+type Expression interface {
+	GetExpressionDataType() dataType
+}
+
+type BoolExpression struct {
+	Value1    RefType
+	Value2    RefType
+	Operation conditionOperation
+}
+
+func (e *BoolExpression) GetExpressionDataType() dataType {
+	return boolType
+}
+
+type NumericExpression struct {
+	Value1    float64
+	Value2    float64
+	operation numericOperation
+}
+
+func (e *NumericExpression) GetExpressionDataType() dataType {
+	return numericType
+}
+
+type StringExpression struct {
 	Value1    string
 	Value2    string
-	Operation string
+	operation stringOperation
 }
 
-// the difference between Field and Param is that Field is just a Field of the state, whereas Param is given by an user input
-// therefore Param requires validation as we need to validate if user value is accepted, whereas Field is always assigned by
-// Game internally so no need to validate
-type Param struct {
-	Name     string
-	DataType dataType
-	// options has to be a ref to a state list field
-	OptionsField string
-	Validator    ParamValidator
+func (e *StringExpression) GetExpressionDataType() dataType {
+	return stringType
 }
 
-type ParamValidator struct {
-	BoolExpressions []ParamValidatorExpression
-	Connectors      []string
+type ListExpression struct {
+	ListRef   RefType
+	Operation listOperation
+	Value     *RefType
 }
 
-// difference with Expression is that here the DataType and Value1 are infered by param, param is always Value1
-type ParamValidatorExpression struct {
-	Value2    string
-	Operation string
+func (e *ListExpression) GetExpressionDataType() dataType {
+	return listType
+}
+
+type Param interface {
+	GetParamDataType() dataType
+}
+
+type StringParam struct {
+	Name       string
+	FixOptions []string
+	RefOptions RefType
+}
+
+func (p *StringParam) GetParamDataType() dataType {
+	return stringType
+}
+
+type NumericParam struct {
+	Name                            string
+	Options                         []float64
+	RefOptions                      RefType
+	ValidationExpressions           []NumericParamValidator
+	ValidationExpressionsConnectors []boolConnector
+}
+
+func (p *NumericParam) GetParamDataType() dataType {
+	return numericType
+}
+
+type BoolParam struct {
+	Name string
+}
+
+func (p *BoolParam) GetParamDataType() dataType {
+	return boolType
+}
+
+type NumericParamValidator struct {
+	Value2    RefType
+	Operation conditionOperation
+}
+
+type ValueType interface {
+	GetDataType() dataType
+}
+
+type StringType struct {
+	Value string
+}
+
+func (t *StringType) GetDataType() dataType {
+	return stringType
+}
+
+type NumericType struct {
+	Value float64
+}
+
+func (t *NumericType) GetDataType() dataType {
+	return numericType
+}
+
+type BoolType struct {
+	Value bool
+}
+
+func (t *BoolType) GetDataType() dataType {
+	return stringType
+}
+
+type RefType struct {
+	// stores the compositions like 'State' 'Obj1' 'field1'
+	VariableComposition []string
+}
+
+func (t *RefType) GetDataType() dataType {
+	return refType
+}
+
+type ObjectType struct {
+	Fields map[string]ValueType
+}
+
+func (t *ObjectType) GetDataType() dataType {
+	return objType
+}
+
+type ListType struct {
+	Type   dataType
+	Values []ValueType
+}
+
+func (t *ListType) GetDataType() dataType {
+	return listType
 }
